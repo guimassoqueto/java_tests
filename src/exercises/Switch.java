cc6303b648df369131aeb1f2a317978d24ea4b8b
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-16 20:10:25 -0300

    exercise1: Switch module

---
+++ src/exercises/Switch.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        String today = "Tuesday";
+
+        switch (today.toLowerCase()) {
+            case "monday" -> System.out.println("segunda");
+            case "tuesday" -> System.out.println("terca");
+            case "wednesday" -> System.out.println("quarta");
+            case "thursday" -> System.out.println("quinta");
+            case "friday" -> System.out.println("sexta");
+            case "saturday" -> System.out.println("sabado");
+            case "sunday" -> System.out.println("domingo");
+            default -> System.out.println("not a valid day");
+        }
+    }
+}
