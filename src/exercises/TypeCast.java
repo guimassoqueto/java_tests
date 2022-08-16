4069de52ee1d5fa477504fb430ccfa7aebb77677
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-16 19:30:01 -0300

    exercise1: Type Cast

---
+++ src/exercises/TypeCast.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class TypeCast {
+    public static void main(String[] args) {
+        double ten = 10;
+        int three = 3;
+
+        double rest =  (double) (ten /  three); // casting
+
+        System.out.println(rest);
+    }
+}
