414562aa96332d07e8a2d1ead1a74fb5dda2523f
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-16 19:56:38 -0300

    exercise1: FindMax number between two values

---
+++ src/exercises/FindMax.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class FindMax {
+    public static void main(String[] args) {
+        double x = 10.55;
+        double y = 10.556;
+
+        System.out.println(Math.max(x, y));
+    }
+}
