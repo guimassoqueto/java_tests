15bbb4e4bd6a6ebe246cba8d8c64d187b8cf29d8
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-16 20:17:07 -0300

    exercise1: Switch module

---
+++ src/exercises/While.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class While {
+    public static void main(String[] args) {
+        int val = 10000;
+
+        int count = 0;
+        while (count <= val) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
