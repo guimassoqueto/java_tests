eefb9b9eb797f09e6184af82e54ef4950c1e1f3f
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-17 18:58:03 -0300

    exercise1: Wrapper classes

---
+++ src/exercises/WrapperClass.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        Boolean A = true;
+        Character B = 'b';
+        Integer C = 123;
+        Double D = 1.2234;
+        String E = "string";
+
+        // are different
+        boolean a = true;
+        char b = 'b';
+        int c = 123;
+        double d = 1.2234;
+    }
+}
