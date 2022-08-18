c206634cedbe23080e4634d7d04995f3d2ac9be9
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-17 21:05:43 -0300

    object: overwriting toString

---
+++ src/objects/MethodOverwriting.java
@@ -0,0 +1,8 @@
+package objects;
+
+public class MethodOverwriting {
+    public String toString() {
+        String DEFINITION = "This is an instance of MethodOverwriting class.";
+        return DEFINITION;
+    }
+}
