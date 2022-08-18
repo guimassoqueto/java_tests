da526ab106c635e1c4a20b9bcda79a3192d927b4
gui.massoqueto <gui.massoqueto@gmail.com>
2022-08-18 07:54:32 -0300

    exercise: Inheritance, Method override, super

---
+++ src/objects/CarExt.java
@@ -0,0 +1,31 @@
+package objects;
+
+class Vehicle {
+    public float maxSpeedKMH;
+    public String medium;
+    public boolean hasWheels;
+
+    public Vehicle(float maxSpeedKMH, String medium, boolean hasWheels) {
+        this.maxSpeedKMH = maxSpeedKMH;
+        this.medium = medium;
+        this.hasWheels = hasWheels;
+    }
+
+    public void info() {
+        System.out.println("Vehicle info.");
+    }
+}
+
+public class CarExt extends Vehicle {
+    public String brand;
+
+    //super
+    public CarExt(float maxSpeedKMH, String medium, boolean hasWheels, String brand) {
+        super(maxSpeedKMH, medium, hasWheels);
+        this.brand = brand;
+    }
+    @Override
+    public void info() {
+        System.out.println("CarExt info.");
+    }
+}
